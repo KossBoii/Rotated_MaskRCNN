1e56d34684a49ec36f0f03bf808af5b5104f43bf
Long Truong <long.truong0320@gmail.com>
2021-04-05 11:45:37 -0700

    fix util funcs

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ from detectron2.structures import BoxMode
 from detectron2.data import detection_utils as utils
 from detectron2.data import transforms as T
 import torch
+import np
 
 def transform_instance_annotations_rotated(annotation, transforms, image_size, *, keypoint_hflip_indices=None):
     if annotation["bbox_mode"] == BoxMode.XYWHA_ABS:        # rotated bbox
@@ -20,7 +21,7 @@ def custom_rotated_mapper(dataset_dict):
     
     # Data Augmentation 
     augs = T.AugmentationList([
-        T.Resize((600, 800)),
+        T.Resize((300, 400)),
         T.RandomBrightness(0.9, 1.1),
         T.RandomFlip(prob=0.6, horizontal=True, vertical=False),
         T.RandomFlip(prob=0.6, horizontal=False, vertical=True),
@@ -47,7 +48,7 @@ def custom_mapper(dataset_dict):
   image = utils.read_image(dataset_dict["file_name"], format="BGR")
 
   transform_list = [
-                    T.Resize((600, 800)),
+                    T.Resize((300, 400)),
                     T.RandomFlip(prob=0.6, horizontal=True, vertical=False),
                     T.RandomFlip(prob=0.6, horizontal=False, vertical=True),
                     T.RandomBrightness(0.9, 1.1),
