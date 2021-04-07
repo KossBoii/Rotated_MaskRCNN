0fb2ab53fc5da29ebb446996aa3942d08cb05e4a
Long Truong <long.truong0320@gmail.com>
2021-04-07 10:15:34 -0700

    update custom_mapper

--- utils.py
+++ utils.py
@@ -16,32 +16,33 @@ def transform_instance_annotations_rotated(annotation, transforms, image_size, *
         annotation["bbox_mode"] = BoxMode.XYXY_ABS
 
 def custom_rotated_mapper(dataset_dict):
-    dataset_dict = copy.deepcopy(dataset_dict)
+    dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
     image = utils.read_image(dataset_dict["file_name"], format="BGR")
-    
-    # Data Augmentation 
-    augs = T.AugmentationList([
-        T.Resize((300, 400)),
-        T.RandomBrightness(0.9, 1.1),
-        T.RandomFlip(prob=0.6, horizontal=True, vertical=False),
-        T.RandomFlip(prob=0.6, horizontal=False, vertical=True),
-    ])
 
-    input = T.AugInput(image)
-    transforms = augs(input)
-    # image = torch.from_numpy(input.image.transpose(2,0,1))
-    image = torch.as_tensor(input.image.transpose(2, 0, 1))
+    transform_list = [
+                    T.Resize((300, 400)),
+                    T.RandomFlip(prob=0.6, horizontal=True, vertical=False),
+                    T.RandomFlip(prob=0.6, horizontal=False, vertical=True),
+                    T.RandomBrightness(0.9, 1.1),
+    ]
+
+    image, transforms = T.apply_transform_gens(transform_list, image)
+    dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
 
     annos = [
-        transform_instance_annotations_rotated(annotation, transforms, image.shape[1:])
-        for annotation in dataset_dict.pop('annotations')
+        transform_instance_annotations_rotated(obj, transforms, image.shape[:2]) 
+        for obj in dataset_dict.pop("annotations")
+        if obj.get("iscrowd", 0) == 0
     ]
-    instances = utils.annotations_to_instances_rotated(annos, image.shape[1:])
+    
+    instances = utils.annotations_to_instances_rotated(annos, image.shape[:2])
+    dataset_dict["instances"] = utils.filter_empty_instances(instances)
+    return dataset_dict
 
-    return {
-        'image': image,
-        'instances': utils.filter_empty_instances(instances)
-    }
+
+
+
+
 
 def custom_mapper(dataset_dict):
   dataset_dict = copy.deepcopy(dataset_dict)
@@ -63,3 +64,31 @@ def custom_mapper(dataset_dict):
   instances = utils.annotations_to_instances(annos, image.shape[:2])
   dataset_dict["instances"] = utils.filter_empty_instances(instances)
   return dataset_dict
+
+
+    # dataset_dict = copy.deepcopy(dataset_dict)
+    # image = utils.read_image(dataset_dict["file_name"], format="BGR")
+    
+    # # Data Augmentation 
+    # augs = T.AugmentationList([
+    #     T.Resize((300, 400)),
+    #     T.RandomBrightness(0.9, 1.1),
+    #     T.RandomFlip(prob=0.6, horizontal=True, vertical=False),
+    #     T.RandomFlip(prob=0.6, horizontal=False, vertical=True),
+    # ])
+
+    # input = T.AugInput(image)
+    # transforms = augs(input)
+    # # image = torch.from_numpy(input.image.transpose(2,0,1))
+    # image = torch.as_tensor(input.image.transpose(2, 0, 1))
+
+    # annos = [
+    #     transform_instance_annotations_rotated(annotation, transforms, image.shape[1:])
+    #     for annotation in dataset_dict.pop('annotations')
+    # ]
+    # instances = utils.annotations_to_instances_rotated(annos, image.shape[1:])
+
+    # return {
+    #     'image': image,
+    #     'instances': utils.filter_empty_instances(instances)
+    # }
